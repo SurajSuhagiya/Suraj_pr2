607002241ac6b7c33bd6ca8c48141f3e3a46cc22
SurajSuhagiya <136710673+SurajSuhagiya@users.noreply.github.com>
2023-06-28 18:42:54 +0530

    Update SURAJPR2_2.C

--- SURAJPR2_2.C
+++ SURAJPR2_2.C
@@ -9,34 +9,38 @@ float bill,u,i;
 
 clrscr();
 printf("--------------------------------------------------------------\n");
-printf("<<<<<<< ELECTRICITY BILL >>>>>>>>>>\n");
-printf("-------------------------------------------------------------\n");
+printf("<<<<<<<<<<<<<<<<<<<< ELECTRICITY BILL >>>>>>>>>>>>>>>>>>>>>>>>\n");
+printf("--------------------------------------------------------------\n");
 
-printf("-----------------------");
-printf("enter electricity units");
-printf("------------------------\n");
-scanf("%f",&units);
-u = units;
-while (true){
+printf("CHARGES OF PER UNIT");
+printf("For the first 50 units Rs. 0.50/unit\n")
+printf("For the next 100 units Rs. 0.75/unit\n")
+printf("For the next 100 units Rs. 1.20/unit\n");
+printf("For units above 250 Rs. 1.50/unit\n");
 
-   if (units <= 50) {
-	bill = units * 0.5;
-	continue;
+printf("ENTER YOUR ELECTRICITY UNITS:");
+scanf("%f",&bill);
+printf("\n");
 
-   } else if (units <= 150) {
-	bill = 50 * 0.5 + (units - 50) * 0.75;
-	continue;
-   } else if (units <= 250) {
-	bill = 50 * 0.5 + 100 * 0.75 + (units - 150) * 1.2;
-	continue;
-   } else if (units > 250) {
-	bill = 50 * 0.5 + 100 * 0.75 + 100 * 1.2 + (units - 250) * 1.5;
-	bill = bill + bill * 0.2;
-	break;
+if(bill<=50){
+   a=(bill*0.50);
+                          
+       }else if(bill<=150){
+         a=(25 + (bill - 50)*0.75);
+                                
+       }else if(bill<=250){
+         a=(25 + 75 +(bill-150)*1.20);
+                                
+       }else{
+         a=(25 + 75 + 120 + (bill - 250 )*1.50);
+       }
+
+f =a +a*0.2 ;    
+printf(   "Electricity Bill = Rs.  %.1f",f);
+return 0;
+getch();    
 }
-}
 
-printf("answer %f",bill);
-
-getch();
-}
+/*Input : Enter electricity units: 435
+Output : Electricity Bill = Rs. 597.0
+*/
